9709fe7f28c5913245fa2c1beabc58ee067ae94f
Jerome Forissier <jerome@taodyne.com>
2013-08-21 14:37:04 +0200

    Add missing file for 'White' theme.

---
+++ app/view/white/ThemeIcon.js
@@ -0,0 +1,9 @@
+Ext.define('TE.view.white.ThemeIcon', {
+    extend: 'TE.view.ThemeIcon',
+
+    statics: {
+	    ptclassname: 'TE.view.white.PageTemplates',
+    },
+    image: 'app/assets/images/white/white.png',
+    caption: 'White'
+ });
