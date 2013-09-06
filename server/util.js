5a7ceb5fc36fb7ff5135cf5eedca009c962476ef
Jerome Forissier <jerome@taodyne.com>
2013-09-06 11:54:25 +0200

    Handle text-align: {center,left,right} when converting HTML to Tao.

--- server/util.js
+++ server/util.js
@@ -21,11 +21,16 @@ var DomToSlideConverter = (function() {
     var indentString = '    ';
     var lineStart = true;
     var listMode = ''; // '', 'ul', 'ol'
+    var prev = ''; // previous line
 
     var out = '';
 
     function output(txt)
     {
+        // Multiple occurrences of some commands are useless
+        if (txt.trim() === 'paragraph_break' && prev === txt.trim())
+            return;
+
         if (lineStart)
             indent();
         out += txt;
@@ -43,6 +48,47 @@ var DomToSlideConverter = (function() {
         out = '';
         lineStart = true;
         nindent = 1;
+    }
+
+    // Output Tao code for 'style' attribute
+    function convertStyle(style, revert)
+    {
+        revert = (typeof revert !== 'undefined') ? revert : false;
+
+        var styles = style.match(/[^;]+/g); // split at ;, leave out empty elements
+        styles.forEach(function(s) {
+            var col = s.indexOf(':');
+            var name = s.substring(0, col).trim();
+            var value = s.substring(col + 1).trim();
+            switch (name)
+            {
+            case 'text-align':
+                if (value === 'left' || value === 'center' || value === 'right')
+                {
+                    if (revert)
+                    {
+                        output('paragraph_break\n');
+                        value = 'left';
+                    }
+                    output('align_' + value + '\n');
+                }
+                else
+                {
+                    console.log('Unsupported text-align value: ' + value);
+                }
+                break;
+            case 'color':
+                // Ignored because text color overrides are not set by
+                // <div style: 'color: rgb(x,y,z);'>
+                // but by <font color="#abcdef"> which is handled elsewhere.
+                // And, <div style: 'color: rgb(0,0,0);'> is used to restore
+                // the default color, which is done automatically by text_span.
+                break;
+            default:
+                console.log('Style element ignored: ' + name + ': ' + value);
+            }
+        })
+
     }
 
     function convert(dom)
@@ -68,9 +114,16 @@ var DomToSlideConverter = (function() {
             {
             case 'div':
                 output('paragraph_break\n');
+                if (dom.attribs && dom.attribs.style)
+                    convertStyle(dom.attribs.style);
                 dom.children.forEach(function(elt) {
                     convert(elt);
                 });
+                if (dom.attribs && dom.attribs.style)
+                {
+                    output('paragraph_break\n');
+                    convertStyle(dom.attribs.style, true);
+                }
                 break;
             case 'span':
                 if (dom.attribs)
@@ -128,6 +181,8 @@ var DomToSlideConverter = (function() {
                 });
                 break;
             case 'li':
+                if (dom.attribs && dom.attribs.style)
+                    convertStyle(dom.attribs.style);
                 switch (listMode)
                 {
                 case 'ul':
@@ -145,6 +200,8 @@ var DomToSlideConverter = (function() {
                 default:
                     console.log('<li> outside of <ul>/<ol>');
                 }
+                if (dom.attribs && dom.attribs.style)
+                    convertStyle(dom.attribs.style, true);
                 break;
             case 'font':
                 var hasAttr = (dom.attribs && (dom.attribs.face || dom.attribs.color || dom.attribs.size));
@@ -165,7 +222,8 @@ var DomToSlideConverter = (function() {
                 if (dom.attribs && dom.attribs.size)
                 {
                     var scale = 1 + 0.2 * (Number(dom.attribs.size) - 2);
-                    output('font_size ' + scale + ' * theme_size(theme, slide_master, "story")\n')
+                    if (scale != 1)
+                        output('font_size ' + scale + ' * theme_size(theme, slide_master, "story")\n');
                 }
                 dom.children.forEach(function(elt) {
                     convert(elt);
@@ -194,6 +252,13 @@ var DomToSlideConverter = (function() {
 // for use as the body of a "slide" command.
 function htmlToSlideContent(html)
 {
+    // \n sometimes appear in the source html between html elements. They should
+    // be discarder because:
+    // (1) They are not meaningful (the html editor uses <div>'s for paragraph
+    // breaks and <br> for line breaks),
+    // (2) Carriage returns cannot be inserted in the text strings due to the
+    // Tao syntax we use: text "..."
+    html = html.replace(/\n/g, '');
     var handler = new htmlparser.DefaultHandler(function(error, dom) {
         if (error)
         {
