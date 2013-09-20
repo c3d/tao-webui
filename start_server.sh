3083befe2047c2e96887bdde089b59634399c8cc
Jerome Forissier <jerome@taodyne.com>
2013-09-20 16:49:16 +0200

    Prepare integration into Tao Presentations as a subproject: rename Makefile.
    We will soon have a Qt project file (.pro) that will generate a Makefile.

--- start_server.sh
+++ start_server.sh
@@ -2,9 +2,16 @@
 
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd )"
 
-(
+error() {
+  echo $1 >&2
+  exit
+}
+
+pushd . >/dev/null
 cd $SCRIPTPATH/server
 [ -e node_modules ] || npm install
-)
+[ -e www/ext-4 ] || error "ExtJS 4 not found under www/ext-4. Please install it from http://www.sencha.com/products/extjs/download."
+popd >/dev/null
+
 #export PORT=3000
 node $SCRIPTPATH/server/server.js -v $*
