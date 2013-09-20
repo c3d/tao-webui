a9a7b68c5e894beaf8b5b9d5330da09d32e1a41b
Jerome Forissier <jerome@taodyne.com>
2013-09-20 13:50:58 +0200

    Create server/package.json. NPM dependencies are defined there.

--- start_server.sh
+++ start_server.sh
@@ -4,9 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd )"
 
 (
 cd $SCRIPTPATH/server
-for m in express htmlparser ent http-proxy ejs; do
-  [ -e node_modules/$m ] || npm install $m
-done
+[ -e node_modules ] || npm install
 )
 #export PORT=3000
 node $SCRIPTPATH/server/server.js -v $*
