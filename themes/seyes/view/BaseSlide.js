b6c0599392ae0428e314ff991e4656022c37ec8e
Jerome Forissier <jerome@taodyne.com>
2013-11-08 11:28:17 +0100

    Seyes: add BaseSlide.

---
+++ themes/seyes/view/BaseSlide.js
@@ -0,0 +1,5 @@
+Ext.define('TE.themes.seyes.view.BaseSlide', {
+    extend: 'TE.view.PageTemplate',
+    image: 'app/themes/seyes/resources/images/base_slide.png',
+    caption: tr('Base Slide', 'common')
+ });
---
+++ themes/seyes/view/properties/BaseSlide.js
@@ -0,0 +1,3 @@
+Ext.define('TE.themes.seyes.view.properties.BaseSlide', {
+    extend: 'TE.themes.common.view.properties.BaseSlide'
+});
